7709543ba25c1672d73ed3a21359eb6d0dc10553
WENTING SUN <45490316+WentingSun@users.noreply.github.com>
2025-03-18 11:16:53 +0000

    add basic Enemy Script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] int Health;
+    [SerializeField] int MaxHealth;
+
+    public void takeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health < 0)
+        {
+            beKilled();
+        }
+    }
+
+    private void beKilled()
+    {
+        Destroy(this);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Health = MaxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
